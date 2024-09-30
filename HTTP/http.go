df0ac0f24f3f6bb34ad1d0899fb0c294a6137298
Pedro Souza <pedrops244@hotmail.com>
2024-09-30 11:29:03 -0300

    concepts of http methods

---
+++ HTTP/http.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+
+	http.HandleFunc("/home", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Olá mundo!"))
+	})
+	log.Fatal(http.ListenAndServe(":5000", nil))
+}
