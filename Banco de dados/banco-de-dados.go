3be9c039aea65ec79431f9797e8c1f3e32f22bec
Pedro Souza <pedrops244@hotmail.com>
2024-09-30 11:45:27 -0300

    starting database w/ golang

---
+++ Banco de dados/banco-de-dados.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func main() {
+	stringConexao := "golang:golang@/devbook?charset=utf8&parseTime=True&loc=Local"
+	db, erro := sql.Open("mysql", stringConexao)
+	if erro != nil {
+		log.Fatal(erro)
+	}
+	defer db.Close()
+
+	if erro = db.Ping(); erro != nil {
+		log.Fatal(erro)
+	}
+	fmt.Println("Conexão com o banco de dados está aberta")
+
+	linhas, erro := db.Query("select * FROM usuarios")
+	if erro != nil {
+		log.Fatal(erro)
+	}
+	defer linhas.Close()
+}
