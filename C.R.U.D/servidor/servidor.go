07ad3e3b1e506d802fe5bdc32e639261a31fee08
Pedro Souza <pedrops244@hotmail.com>
2024-09-30 18:12:24 -0300

    method getUser indv

--- C.R.U.D/servidor/servidor.go
+++ C.R.U.D/servidor/servidor.go
@@ -6,6 +6,9 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
 )
 
 type usuario struct {
@@ -89,5 +92,36 @@ func BuscarUsuarios(w http.ResponseWriter, r *http.Request) {
 	}
 }
 func BuscarUsuario(w http.ResponseWriter, r *http.Request) {
+	parametros := mux.Vars(r)
 
+	ID, erro := strconv.ParseUint(parametros["id"], 10, 32)
+	if erro != nil {
+		w.Write([]byte("Erro ao converter o parâmetro para inteiro"))
+		return
+	}
+
+	db, erro := banco.Conectar()
+	if erro != nil {
+		w.Write([]byte("Erro ao conectar com o banco de dados"))
+		return
+	}
+
+	linha, erro := db.Query("select * from usuarios where id = ?")
+	if erro != nil {
+		w.Write([]byte("Erro ao buscar o usuário"))
+		return
+	}
+
+	var usuario usuario
+	if linha.Next() {
+		if erro := linha.Scan(&usuario.ID, &usuario.Nome, &usuario.Email); erro != nil {
+			w.Write([]byte("Erro ao conectar com o banco de dados"))
+			return
+		}
+	}
+
+	if erro := json.NewEncoder(w).Encode(usuario); erro != nil {
+		w.Write([]byte("Erro ao conectar o usuário para JSON"))
+		return
+	}
 }
