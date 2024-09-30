4befed8d2647968f675850d5cc9c542e8b20d0ce
Pedro Souza <pedrops244@hotmail.com>
2024-09-30 18:00:34 -0300

    starting the database models with golang, creating the primary routes, basic CRUD

---
+++ C.R.U.D/servidor/servidor.go
@@ -0,0 +1,93 @@
+package servidor
+
+import (
+	"crud/banco"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+type usuario struct {
+	ID    uint32 `json:"id"`
+	Nome  string `json:"nome"`
+	Email string `json:"email"`
+}
+
+func CriarUsuario(w http.ResponseWriter, r *http.Request) {
+	corpoRequisicao, erro := io.ReadAll(r.Body)
+	if erro != nil {
+		w.Write([]byte("Falha ao ler o corpo da requisição"))
+		return
+	}
+
+	var usuario usuario
+
+	if erro = json.Unmarshal(corpoRequisicao, &usuario); erro != nil {
+		w.Write([]byte("Erro ao converter o usuário para struct"))
+		return
+	}
+	db, erro := banco.Conectar()
+	if erro != nil {
+		w.Write([]byte("Erro ao conectar no banco de dados"))
+		return
+	}
+	defer db.Close()
+
+	statement, erro := db.Prepare("inser into usuarios (nome, email) values (?, ?)")
+	if erro != nil {
+		w.Write([]byte("Erro ao criar o statement"))
+		return
+	}
+	defer statement.Close()
+
+	insercao, erro := statement.Exec(usuario.Nome, usuario.Email)
+	if erro != nil {
+		w.Write([]byte("Erro ao criar o usuário"))
+		return
+	}
+
+	idInserido, erro := insercao.LastInsertId()
+	if erro != nil {
+		w.Write([]byte("Erro ao obter o id inserido"))
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte(fmt.Sprintf("Usuário inserido com sucesso! Id: %d", idInserido)))
+}
+
+func BuscarUsuarios(w http.ResponseWriter, r *http.Request) {
+	db, erro := banco.Conectar()
+	if erro != nil {
+		w.Write([]byte("Erro ao conectar com o banco de dados"))
+		return
+	}
+	defer db.Close()
+
+	linhas, erro := db.Query("select * from usuarios")
+	if erro != nil {
+		w.Write([]byte("Erro ao buscar os usuários"))
+		return
+	}
+	defer linhas.Close()
+
+	var usuarios []usuario
+	for linhas.Next() {
+		var usuario usuario
+
+		if erro := linhas.Scan(&usuario.ID, &usuario.Nome, &usuario.Email); erro != nil {
+			w.Write([]byte("erro ao escanear o usuário"))
+			return
+		}
+		usuarios = append(usuarios, usuario)
+	}
+	w.WriteHeader(http.StatusOK)
+	if erro := json.NewEncoder(w).Encode(usuarios); erro != nil {
+		w.Write([]byte("Erro ao converter os usuários para JSON"))
+		return
+	}
+}
+func BuscarUsuario(w http.ResponseWriter, r *http.Request) {
+
+}
